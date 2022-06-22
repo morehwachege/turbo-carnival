f1e6e79c2a19e22c38955dd48366da4a15bcf391
morehwachege <antony123muriithi@gmail.com>
2022-06-23 01:03:51 +0300

    intro to js

---
+++ script.js
@@ -0,0 +1,7 @@
+let name = document.getElementById('name');
+let height = document.getElementById('height');
+let country = document.getElementById('country');
+
+height.textContent = "5ft 11";
+country.textContent = "Kenya";
+name.textContent = "Antony Muriithi Gakuru";
