1d5678da0a657f0ee12d1f65d22c6cbf1f6c7d1b
morehwachege <antony123muriithi@gmail.com>
2022-06-23 00:56:47 +0300

    clear record

--- integer_addition.py
+++
@@ -1,7 +0,0 @@
-print("Enter the numbers you want to add.")
-num1 = int(input("Enter first number"))
-num2 = int(input("Enter second number"))
-
-addition = num1 + num2 
-
-print(addition)
--- words.py
+++
@@ -1,4 +0,0 @@
-sentence  = "This is my name, I am King Zodd"
-list_of_words = sentence.split(" ")
-words = len(list_of_words)
-print(words ," words")
